76882f63c665489bd37bfa372f1e5ed2e109df60
Ys-Zhou <zhouyan1993ys@gmail.com>
2017-11-24 16:42:51 +0900

    171124

---
+++ Combination_Sum.py
@@ -0,0 +1,35 @@
+# 168 / 168 test cases passed.
+# Runtime: 375 ms
+
+import copy
+
+
+class Solution(object):
+    def __init__(self):
+        self.result = []
+        self.sortedCand = []
+        self.length = None
+
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        self.sortedCand = sorted(candidates)
+        self.length = len(candidates)
+        self.searchPath(0, target, [])
+        return self.result
+
+    def searchPath(self, nowIndex, target, path):
+        if nowIndex < self.length:
+            if target == 0:
+                self.result.append(path)
+            elif target > 0:
+                # Choose nowIndex
+                pathC = copy.copy(path)
+                pathC.append(self.sortedCand[nowIndex])
+                self.searchPath(nowIndex, target - self.sortedCand[nowIndex], pathC)
+                # Not choose nowIndex
+                self.searchPath(nowIndex + 1, target, path)
+        return
