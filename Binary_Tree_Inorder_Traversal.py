9dbeeec231faef3f6f7eff437cda095b2d934b50
Ys-Zhou <zhouyan1993ys@gmail.com>
2017-11-30 02:21:52 +0900

    Y. Zhou

---
+++ Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,27 @@
+# 68 / 68 test cases passed.
+# Runtime: 29 ms
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return res
