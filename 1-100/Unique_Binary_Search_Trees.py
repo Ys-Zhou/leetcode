df3495d0bc504237305b11974906f27f8fd124e9
Y. Zhou <zhouyan1993ys@gmail.com>
2018-12-04 18:23:21 +0900

    Y. Zhou

--- 1-100/Unique_Binary_Search_Trees.py
+++ 1-100/Unique_Binary_Search_Trees.py
@@ -1,4 +1,4 @@
-# Correct but TLE
+# Runtime: 20 ms, faster than 94.24% of Python online submissions
 
 
 class Solution(object):
@@ -7,10 +7,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n <= 1:
+        if n < 2:
             return 1
-        return reduce(lambda x, y: x + y, [self.numTrees(i) * self.numTrees(n - 1 - i) for i in xrange(n)])
-
-
-s = Solution()
-print s.numTrees(9)
+        dp = [0 for _ in xrange(n + 1)]
+        dp[0] = dp[1] = 1
+        for x in xrange(2, n + 1):
+            for left in xrange(0, x):
+                dp[x] += dp[left] * dp[x - left - 1]
+        return dp[n]
